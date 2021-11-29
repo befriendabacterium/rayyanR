7f0dd4398d4bbdf5830b9c254edc5b4dd0fc10f0
Matt Lloyd Jones <42059798+befriendabacterium@users.noreply.github.com>
2021-11-29 13:46:43 +0000

    Updated the rayyan.R script in Matt G's branch

--- R/parse_rayyan.R
+++ R/parse_rayyan.R
@@ -8,21 +8,21 @@
 #' @export
 parse_rayyan<-function(rayyan_df){
   
-  #1. Split the Rayyan markes (in the notes column) up into a list of their constituent parts (Inclusion decision, Label(s) and Exclusion Reasons)
+  #1. Remove any old inclusion labels from previous screenings abstract screening (parsed as paragraphs)
+  rayyan_df$notes<-sub("<p>.*</p> ","",rayyan_df$notes)
+  
+  #2. Split the Rayyan markers (in the notes column) up into a list of their constituent parts (Inclusion decision, Label(s) and Exclusion Reasons)
   rayyanmarkers_split<-strsplit(rayyan_df$notes, split='| ', fixed=TRUE)  
   
-  #2. Remove any relictual inclusion labels from previous screenings which may be found as free text in the Labels or parsed as HTML paragraphs (latter is a weird bug in the Rayyan output?)
+  #3. Remove any old inclusion labels from previous screenings which may be found as free text in the Labels
   for (e in 1:length(rayyanmarkers_split)){
     
     #for each label vector in each element of the string, remove the old inclusion bit
     rayyanmarkers_split[[e]][grep('RAYYAN-LABELS|RAYYAN-EXCLUSION',rayyanmarkers_split[[e]])]<-sub("RAYYAN-INCLUSION.*", "", rayyanmarkers_split[[e]][grep('RAYYAN-LABELS|RAYYAN-EXCLUSION',rayyanmarkers_split[[e]])])
     
-    #remove any old inclusion labels from abstract screening (parsed as paragraphs)
-    rayyanmarkers_split<-lapply(rayyanmarkers_split, function(x) sub("<p>.*</p> ", "",x))
-    
   }
   
-  #3. Create separate vectors/new columns for Inclusion decisions, Labels and Exclusion decisions
+  #4. Create separate vectors/new columns for Inclusion decisions, Labels and Exclusion decisions
   #grep the inclusion decisions from the list
   rayyan_df$rayyan_decisions<-as.character(lapply(rayyanmarkers_split, function(x) x[grep('RAYYAN-INCLUSION:',x)]))
   #tidying up: remove weird paragraph marker bit from inclusion decisions
@@ -32,23 +32,23 @@ parse_rayyan<-function(rayyan_df){
   #grep the exclusion reasons from the list
   rayyan_df$rayyan_exclusionreasons<-as.character(lapply(rayyanmarkers_split, function(x) x[grep('RAYYAN-EXCLUSION-REASONS:',x)]))
   
-  #4. Remove the Rayyan label bit
+  #5. Remove the Rayyan label bit
   rayyan_df$rayyan_decisions<-gsub('RAYYAN-INCLUSION:','',rayyan_df$rayyan_decisions)
   rayyan_df$rayyan_labels<-gsub('RAYYAN-LABELS:','',rayyan_df$rayyan_labels)
   rayyan_df$rayyan_exclusionreasons<-gsub('RAYYAN-EXCLUSION-REASONS:','',rayyan_df$rayyan_exclusionreasons)
   
-  #5. Do some tidying up where there's no label
+  #6. Do some tidying up where there's no label
   rayyan_df$rayyan_decisions[rayyan_df$rayyan_decisions=='character(0)']<-NA
   rayyan_df$rayyan_labels[rayyan_df$rayyan_labels=='character(0)']<-NA
   rayyan_df$rayyan_exclusionreasons[rayyan_df$rayyan_exclusionreasons=='character(0)']<-NA
   
-  #6. Identify the three types conflicts (N.B. We include Maybes-Inclusion/Exclusion as 'conflicts' unlike Rayyan). Code is quite janky right now.
+  #7. Identify the three types conflicts (N.B. We include Maybes-Inclusion/Exclusion as 'conflicts' unlike Rayyan). Code is quite janky right now.
   conflicts1<-grep("Excluded.*Included|Included.*Excluded",rayyan_df$rayyan_decisions)
   conflicts2<-grep("Excluded.*Maybe|Maybe.*Excluded",rayyan_df$rayyan_decisions)
   conflicts3<-grep("Included.*Maybe|Maybe.*Included",rayyan_df$rayyan_decisions)
   conflicts<-c(conflicts1,conflicts2,conflicts3)
   
-  #7. Create new column identifying the final decision
+  #8. Create new column identifying the final decision
   
   #create blank final decision vector
   rayyan_df$finaldecision<-rep(NA,nrow(rayyan_df))
@@ -71,7 +71,7 @@ parse_rayyan<-function(rayyan_df){
   #change final decision to Excluded for these
   rayyan_df$finaldecision[maybe]<-'Maybe'
   
-  #8. Return the cleaned dataframe
+  #9. Return the cleaned dataframe
   return(rayyan_df)
   
 }
