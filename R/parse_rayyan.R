0c97e9647ebc46f95b00e96b0da292fa71f8fdd3
Matthew Grainger <DrMattG@users.noreply.github.com>
2023-10-11 20:52:34 +0200

    Update parse_rayyan.R
    Use equals to or greater than (>=)

--- R/parse_rayyan.R
+++ R/parse_rayyan.R
@@ -3,7 +3,7 @@
 #' @details big ole messy function (beta) to remove any old inclusion labels from abstract screening (found in the labels or parsed as html paragraphs)
 #' @author Matt Lloyd Jones
 #' @param rayyan_df A rayyan exported bibliography file
-#' @param nR Minimum number of reviewers for a decision (default is greater than 2)
+#' @param nR Minimum number of reviewers for a decision (default is equals to or greater than 2)
 #' @return A dataframe 
 #' @export
 parse_rayyan<-function(rayyan_df, nR=2){
@@ -57,17 +57,17 @@ parse_rayyan<-function(rayyan_df, nR=2){
   rayyan_df$finaldecision[conflicts]<-'Conflict'
   
   #count where 2 reviewers decided include
-  included<-which(lengths(regmatches(rayyan_df$rayyan_decisions,gregexpr('Included',rayyan_df$rayyan_decisions)))>nR)
+  included<-which(lengths(regmatches(rayyan_df$rayyan_decisions,gregexpr('Included',rayyan_df$rayyan_decisions)))>=nR)
   #change final decision to Included for these
   rayyan_df$finaldecision[included]<-'Included'
   
   #count where 2 reviewers decided exclude
-  excluded<-which(lengths(regmatches(rayyan_df$rayyan_decisions,gregexpr('Excluded',rayyan_df$rayyan_decisions)))>nR)
+  excluded<-which(lengths(regmatches(rayyan_df$rayyan_decisions,gregexpr('Excluded',rayyan_df$rayyan_decisions)))>=nR)
   #change final decision to Excluded for these
   rayyan_df$finaldecision[excluded]<-'Excluded'
   
   #count where 2 reviewers decided Maybe
-  maybe<-which(lengths(regmatches(rayyan_df$rayyan_decisions,gregexpr('Maybe',rayyan_df$rayyan_decisions)))>nR)
+  maybe<-which(lengths(regmatches(rayyan_df$rayyan_decisions,gregexpr('Maybe',rayyan_df$rayyan_decisions)))>=nR)
   #change final decision to Excluded for these
   rayyan_df$finaldecision[maybe]<-'Maybe'
   
