f3e893500fec97cf128a607e3d5d06db0f899b9b
Matt Lloyd Jones <42059798+befriendabacterium@users.noreply.github.com>
2023-12-15 16:03:17 +0000

    Add unnest_all function
    For unnesting datadrames from jsons

---
+++ R/unnest_all.R
@@ -0,0 +1,24 @@
+#' unnest_all
+#'
+#' recursively unnests all the list columns in a dataframe. borrowed from:
+#' https://stackoverflow.com/questions/63786411/how-to-unnest-wider-with-loop-over-all-the-columns-containing-lists
+#'
+#' @param df the dataframe that came from the json file
+#'
+#' @keywords internal
+#'
+#' @return the R object containing the unnested dataframe
+unnest_all <- function(df) {
+  list_columns <- df %>% keep(is.list) %>% names()
+  
+  if (length(list_columns) == 0) {
+    return(df)
+  }
+  
+  for (list_column in list_columns) {
+    df <-
+      df %>%
+      unnest_wider(list_column, names_sep = "_")
+  }
+  unnest_all(df)
+}
