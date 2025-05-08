fd1e81752b98db6e7a23b6092c4c06d91be13435
Matt Lloyd Jones <42059798+befriendabacterium@users.noreply.github.com>
2025-05-08 14:12:22 +0100

    Get it working with different types of input
    Using 'stages' again temporarily

--- R/reviewresults.R
+++ R/reviewresults.R
@@ -1,24 +1,57 @@
 #' reviewresults
 #'
 #' gets a review from the rayyan API and outputs the results a tidied R object
-#'
-#' @param api_tokens the api environment from load_tokens_and_env()
-#' or login_tokens_and_env()
+#' @param api_tokens the api environment from load_tokens_and_env() or login_tokens_and_env()
 #' @param review_id the rayyan ID of the review to get - this can be obtained via
-#' get_reviews
-#' @param fulltextreview_id if there is a separate review for the full text (rather than it being in), specify it here'.
+#' @param stages specify if both stages present in the review
+#' @param recordsreview_id if there is a separate review for the records/abstract (rather than it being in), specify it here'.
+#' @param reportsreview_id if there is a separate review for the reports/full text (rather than it being in), specify it here'.
 #' @keywords internal
 #' @return the R object containing the result of the API call #' 
 #' @export
 
-reviewresults <- function(api_tokens, review_id, fulltextreview_id=NULL) {
+reviewresults <- function(api_tokens, review_id=NULL, stages=c('both','records'), recordsreview_id=NULL, reportsreview_id=NULL) {
   
-  if (is.null(fulltextreview_id)){
-  #get JSON bodies for both stages  
-  body_records<-reviewresults_req_records(api_tokens, review_id)
-  body_reports<-reviewresults_req_reports(api_tokens, review_id='1441821') #temporarily specify review id as currently only works with this function
-  bodies<-list(body_records,body_reports)
+  bodies<-list()
   
+  #if there is only a review id with two stages, get results for both stages
+  if (!is.null(review_id)&stages=='both'){
+    body_records<-reviewresults_req_records(api_tokens, review_id)
+    body_reports<-reviewresults_req_reports(api_tokens, review_id='1441821') #temporarily specify review id as currently only works with this function
+    bodies<-list(body_records, body_reports)
+    names(bodies)<-c('records','reports')
+    
+  }
+    
+  #if there is only a review id with only records stage, get results for the records stage
+  if (!is.null(review_id)&stages=='one'){
+    #reset bodies
+    bodies<-list() 
+    #get JSON bodies for both stages  
+    body_records<-reviewresults_req_records(api_tokens, review_id)
+    bodies[[length(bodies)+1]]<-body_records
+    names(bodies)[[length(bodies)]]<-'reports'
+  }
+  
+  #if there is a records id, get results for the records stage
+  if (!is.null(recordsreview_id)){
+    #reset bodies
+    bodies<-list() 
+    #get JSON bodies for both stages  
+    body_records<-reviewresults_req_records(api_tokens, review_id=recordsreview_id)
+    bodies[[length(bodies)+1]]<-body_records
+    names(bodies)[[length(bodies)]]<-'records'
+  }
+  
+  #if there is a reports id, get results for the reports stage
+  if (!is.null(reportsreview_id)){
+    #get JSON bodies for both stages  
+    body_reports<-reviewresults_req_reports(api_tokens, review_id='1441821') #temporarily specify review id as currently only works with this function
+    bodies[[length(bodies)+1]]<-body_reports
+    names(bodies)[[length(bodies)]]<-'reports'
+  }
+
+  #then apply to all bodies  
   for (i in 1:length(bodies)){
     
     #parse json body (list format) to data.frame with nested lists
@@ -40,7 +73,7 @@ reviewresults <- function(api_tokens, review_id, fulltextreview_id=NULL) {
     review_results_df<-reviewresults_calculateconsensus(review_results_df = review_results_df)
     
     #if records/abstracts stage
-    if (i==1){
+    if (names(bodies)[i]=='records'){
     #Rename 'customizations' columns to a more tidy format 
     ##replace 'customizations_included_' with 'record_decision_'
     colnames(review_results_df) <- gsub("customizations_included_", "record_decision_", colnames(review_results_df))
@@ -53,7 +86,7 @@ reviewresults <- function(api_tokens, review_id, fulltextreview_id=NULL) {
     }
     
     #if reports/full texts stage
-    if (i==2){
+    if (names(bodies)[i]=='reports'){
     #Rename 'customizations' columns to a more tidy format 
     ##replace 'customizations_included_' with 'record_decision_'
     colnames(review_results_df) <- gsub("customizations_included_", "report_decision_", colnames(review_results_df))
@@ -65,16 +98,24 @@ reviewresults <- function(api_tokens, review_id, fulltextreview_id=NULL) {
     review_results_df_reports<-review_results_df
     }
   }
-    
+   
+  #if there is a records id AND a reports ID, bind into one
+  if (stages=='both'){
     #merge stages into one df
     review_results_df <- dplyr::left_join(review_results_df_records, review_results_df_reports)
+  }
+  
+  #if there is a records id AND a reports ID, bind into one
+    if (!is.null(recordsreview_id)&!is.null(reportsreview_id)){
+    #merge stages into one df
+    review_results_df <- dplyr::left_join(review_results_df_records, review_results_df_reports)
+    }
     
     #review_info<-get_review_info_raw(api_tokens, review_id)
     #NB DISABLED RENAMING AS NOT WORKING - CHECK RENAME_INCLUDED_COLS_NAMES/VALUES() FUNCTIONS
     #review_results_df<-rename_included_cols_names(review_results_df = review_results_df, review_info=review_info, rename_with = 'name')
     #review_results_df<-rename_included_cols_values(review_results_df = review_results_df)
     #if one reviewer is NA, remove
-    }
   
   return(review_results_df)
 }
