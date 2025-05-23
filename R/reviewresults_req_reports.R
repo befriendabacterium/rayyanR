4b8ecc6839855a1f226d0784ec2ca1d157536988
Matt Lloyd Jones <42059798+befriendabacterium@users.noreply.github.com>
2025-05-23 09:01:45 +0100

    Implement actual full text stage fetching
    - make API call to get stages metadata
    - input this into API call to get review data in order to get full text stage

--- R/reviewresults_req_reports.R
+++ R/reviewresults_req_reports.R
@@ -12,7 +12,17 @@
 #' @return the R object containing the result of the API call
 #' @export
 reviewresults_req_reports <- function(api_tokens, review_id) {
-  reviews_route <- paste0("/api/v1/reviews/", review_id, "/results/?extra[stages]=__SYSTEM__STAGE__113306")
+  
+  stages_route<-paste0("/api/v1/reviews/", review_id, "/facets?facets[stages]=1")
+  url <- paste0("https://rayyan.ai",  stages)
+  req <- httr2::request(url)
+  req <- httr2::req_auth_bearer_token(req, api_tokens$access_token)
+  resp <- httr2::req_perform(req)
+  body <- httr2::resp_body_json(resp)
+  limbs <- unlist(body[[1]])
+  filter_key <- limbs[names(limbs)=='filter_key']
+  
+  reviews_route <- paste0("/api/v1/reviews/", review_id, "/results/?extra[stages]=", filter_key)
   url <- paste0("https://rayyan.ai",  reviews_route)
   req <- httr2::request(url)
   req <- httr2::req_auth_bearer_token(req, api_tokens$access_token)
--- test/reviewresults_tester.R
+++ test/reviewresults_tester.R
@@ -17,6 +17,11 @@ reports_results<-reviewresults(api_tokens = api_tokens, reportsreview_id = "1445
 
 records_and_reports_results<-reviewresults(api_tokens = api_tokens, recordsreview_id = "1441821", reportsreview_id = "1445423", stages='one')
 
+records_and_reports_results<-records_and_reports_results %>% rename(abstract=abstracts_content)
+records_and_reports_results<-records_and_reports_results %>% rename(author=authors)
+records_and_reports_results<-records_and_reports_results %>% rename(DOI=doi)
+
+synthesisr::write_refs(records_and_reports_results,file='recs_and_reps.bib', format='bib')
 
 devtools::document()
 #usethis document package or in API
